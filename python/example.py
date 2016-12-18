76c5f8e2df323d4923682746bb1fae3448d3d5a7
Jonathan Sadan <jonathan@cloudshare.com>
2016-12-18 11:41:23 +0200

    Add python example

--- python/example.py
+++ python/example.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 import cssdk, time
+from collections import namedtuple
+
+CartItemType = namedtuple("CartItemType", ["BASED_ON_BP", "ADD_TEMPLATE_VM"])(1,2)
+TemplateType = namedtuple("TemplateType", ["BLUEPRINT", "VM"])(0,1)
 
 API_ID = "5VLLDABQSBESQSKY"
 API_KEY = "4P3RuSCfFbLQvqJqrBWWrxcxIjZHdlz1CkFqQR4jkIftn3C6wTGfcTawQNMKshUo"
 
 def main():
+	example1_execute_command_on_machine()
+
+def example1_execute_command_on_machine():
 	env = get_first_env()
 	machine = get_first_machine(env)
 	execution = execute_path(machine, "echo hello world")
@@ -13,6 +20,59 @@ def main():
 		time.sleep(5)
 		execution_status = get_execution_status(machine, execution)
 	print "Execution finished! output:\n%s" % execution_status['standardOutput']
+
+def example2_create_custom_environment():
+	project_id = get_first_project_id()
+	miami_region_id = get_miami_region_id()
+	template_vm_id = get_first_template_vm_id()
+	name = create_environment_name()
+	env = create_environment(name, project_id, miami_region_id, template_vm_id)
+	print "New environment ID: " + env["environmentId"]
+	print "New environment Name: " + name
+	print "(This new environment is preparing, to avoid unwanted charges log to use.cloudshare.com and delete the environment)"
+
+def get_first_project_id():
+	projects = get("projects")
+	if len(projects) == 0:
+		raise Exception("No projects found")
+	return projects[0]["id"]
+
+def get_miami_region_id():
+	regions = [r for r in get("regions") if r["name"] == "Miami"]
+	if len(regions) == 0:
+		raise Exception("'Miami' region not found")
+	return regions[0]["id"]
+
+def get_first_template_vm_id():
+	templates = get("templates", {
+		"templateType": TemplateType.VM,
+		"take": 1
+	})
+	if len(templates) == 0:
+		raise Exception("No VM templates found")
+	return templates[0]["id"]
+
+def create_environment(name, projectId, regionId, templateVmId):
+	return post("envs", {
+		"environment": {
+			"name": name,
+			"description": "Environment created from API example",
+			"projectId": projectId,
+			"policyId": None,
+			"regionId": regionId
+		},
+		"itemsCart": [
+			{
+				"type": CartItemType.ADD_TEMPLATE_VM,
+				"name": "123",
+				"description": "123123",
+				"templateVmId": templateVmId
+			}
+		]
+	})
+
+def create_environment_name():
+	return "API Example Environment - " + get_timestamp()
 
 def get_first_env():
 	envs = get('envs/')
@@ -44,7 +104,7 @@ def get_execution_status(machine, execution):
 	return get("vms/actions/checkExecutionStatus", {
 		'vmId': machine['id'],
 		'executionId': execution['executionId']
-	});
+	})
 
 def post(path, content=None):
 	return request('POST', path, content=content)
@@ -63,6 +123,9 @@ def request(method, path, queryParams=None, content=None):
 	if res.status / 100 != 2:
 		raise Exception('{} {}'.format(res.status, res.content['message']))
 	return res.content
+
+def get_timestamp():
+	return str(int(time.time()))
 
 if __name__ == "__main__":
 	main()
