1c145ce0e266b4d404c43c7d957372ba8d7faad6
Assaf Lavie <me@assaflavie.com>
2016-12-28 19:37:03 +0200

    up version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup
 setup(
     name='cloudshare',
     packages=['cloudshare'],
-    version='0.1',
+    version='0.1.1',
     description='A python wrapper for the CloudShare REST API.',
     author='Jonathan Sadan',
     author_email='jonathan@cloudshare.com',
     url='https://github.com/cloudshare/cloudshare-py-sdk',
-    download_url='https://github.com/cloudshare/cloudshare-py-sdk/archive/0.1.tar.gz',
     keywords=['cloudshare', 'cloud', 'SDK', 'REST', 'API'],
     classifiers=[],
 )
