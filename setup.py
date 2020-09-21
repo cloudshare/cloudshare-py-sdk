0ddc84b780336be81bd8aa47d56fb5bc314924b8
Jonathan Sadan <jonathan@cloudshare.com>
2020-09-21 09:18:59 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
-setup(
+import setuptools
+
+setuptools.setup(
     name='cloudshare',
-    packages=['cloudshare'],
+    packages=setuptools.find_packages(),
     version='0.1.2',
     description='A python wrapper for the CloudShare REST API.',
     author='Jonathan Sadan',
@@ -9,4 +10,5 @@ setup(
     url='https://github.com/cloudshare/cloudshare-py-sdk',
     keywords=['cloudshare', 'cloud', 'SDK', 'REST', 'API'],
     classifiers=[],
+    python_requires='>=3.6'
 )
